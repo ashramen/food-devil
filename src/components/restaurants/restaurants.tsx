e640f2a316ee76350b1f47e2e2b1db1fabb99c0c
Aqib Mahfuz <aqib.mahfuz@duke.edu>
2021-10-19 12:49:03 -0400

    Added header to restaurants page

--- src/components/restaurants/restaurants.tsx
+++ src/components/restaurants/restaurants.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+
 import Sidebar from '../sidebar/sidebar';
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 class Restaurants extends React.Component{
 
   render() {
     return (
-        <>
-          <Sidebar page='restaurants'/>
-          <h1>Restaurants</h1>
-        </>
+        <Box mx={2}>
+          <>
+            <Sidebar page='restaurants'/>
+            <Grid container mt={15}>
+              <Grid item xs={6}>
+                <div className='title'>Restaurants</div>
+              </Grid>
+            </Grid>
+          </>
+        </Box>
     );
   }
 }
