12af1e99c086dfea5803308f36959b735a1aa358
SamZhou001 <yuhao.zhou@duke.edu>
2021-11-01 23:48:54 -0400

    Implement and test all API routes

--- src/api/getFoods.tsx
+++
@@ -1,5 +0,0 @@
-import { FOODS, fetchData } from './constants';
-
-export const getAllFoods = async (token: string) => {
-    return fetchData(FOODS, 'GET', token);
-}
--- src/api/restaurants.tsx
+++ src/api/restaurants.tsx
@@ -1,5 +1,9 @@
-import { RESTAURANTS, fetchData } from './constants';
+import { RESTAURANTS, GET_RESTAURANT, fetchData } from './constants';
 
 export const getRestaurants = async (token: string) => {
     return fetchData(RESTAURANTS, 'GET', token);
+}
+
+export const getRestaurant = async (id: string, token: string) => {
+    return fetchData(GET_RESTAURANT(id), 'GET', token);
 }
