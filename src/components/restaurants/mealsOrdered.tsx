6b0c8bdb028dfcfece444d5e23a3020a36b1f780
bluqiu001 <56478564+bluqiu001@users.noreply.github.com>
2021-11-12 15:17:55 -0500

    Restaurants Detail Page (#33)
    * review table page
    
    * meals ordered page
    
    * image and spacing
    
    Co-authored-by: Billy Luqiu <wyl6@duke.edu>

---
+++ src/components/restaurants/mealsOrdered.tsx
@@ -0,0 +1,222 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Box from '@mui/material/Box';
+import { visuallyHidden } from '@mui/utils';
+import SearchBar from "material-ui-search-bar";
+
+import { connect, ConnectedProps } from 'react-redux';
+import { State } from '../../store/index';
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof any>(
+    order: Order,
+    orderBy: Key,
+): (
+        a: { [key in Key]: number | string },
+        b: { [key in Key]: number | string },
+    ) => number {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) {
+            return order;
+        }
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+
+
+interface Column {
+    id: 'meal' | 'date';
+    label: string;
+    minWidth?: number;
+    maxWidth?: number;
+    align?: 'right';
+    format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+    { id: 'meal', label: 'Meal', minWidth: 250, maxWidth: 300 },
+    { id: 'date', label: 'Date', minWidth: 10, maxWidth: 100 }
+];
+
+interface Data {
+    meal: string;
+    date: string;
+    id: number;
+}
+
+function createData(
+    meal: string,
+    date: string,
+    id: number,
+): Data {
+    return { meal, date, id };
+}
+
+const dataRows: Data[] = [
+    createData("Egg and Cheese Burrito", "12/10/21", 1),
+    createData(" Cheese Burrito", "12/10/21", 2),
+    createData("Egg and Cheese Burrito", "12/10/21", 3),
+    createData("Egg  Burrito", "12/10/21", 4),
+    createData("Egg  Burrito", "12/10/21", 5),
+    createData("Sandwich", "12/10/21", 6)
+];
+
+
+interface MealsOrdered extends PropsFromRedux {
+    name: string;
+}
+
+function MealsOrdered(props: MealsOrdered) {
+    const [rows, setRows] = React.useState<Data[]>(dataRows);
+
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof Data>('meal');
+    const [page, setPage] = React.useState(0);
+    const [searched, setSearched] = React.useState<string>("");
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const requestSearch = (searchedVal: string) => {
+        const filteredRows = dataRows.filter((row) => {
+            return row.meal.toLowerCase().includes(searchedVal.toLowerCase());
+        });
+        setRows(filteredRows);
+    };
+
+    const cancelSearch = () => {
+        setSearched("");
+        requestSearch(searched);
+    };
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        console.log(rows);
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    const handleRequestSort = (
+        event: React.MouseEvent<unknown>,
+        property: keyof Data,
+    ) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const createSortHandler =
+        (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+            handleRequestSort(event, property);
+        };
+
+    return (
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+            <SearchBar
+                value={searched}
+                onChange={(searchVal) => requestSearch(searchVal)}
+                onCancelSearch={() => cancelSearch()}
+            />
+            <TableContainer sx={{ maxHeight: 800 }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((headCell) => (
+                                <TableCell
+                                    key={headCell.id}
+                                    align={headCell.align}
+                                    sortDirection={orderBy === headCell.id ? order : false}
+                                >
+                                    <TableSortLabel
+                                        active={orderBy === headCell.id}
+                                        direction={orderBy === headCell.id ? order : 'asc'}
+                                        onClick={createSortHandler(headCell.id)}
+                                    >
+                                        {headCell.label}
+                                        {orderBy === headCell.id ? (
+                                            <Box component="span" sx={visuallyHidden}>
+                                                {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                            </Box>
+                                        ) : null}
+                                    </TableSortLabel>
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {stableSort(rows, getComparator(order, orderBy))
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => {
+                                return (
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                        {columns.map((column) => {
+                                            const value = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={column.align}>
+                                                    {column.format && typeof value === 'number'
+                                                        ? column.format(value)
+                                                        : value}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                );
+                            })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 20]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
+    );
+}
+
+const mapStateToProps = (state: State) => ({
+    loggedIn: state.logIn.loggedIn,
+    token: state.logIn.token,
+    userId: state.logIn.userId,
+});
+
+
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+export default connector(MealsOrdered);
