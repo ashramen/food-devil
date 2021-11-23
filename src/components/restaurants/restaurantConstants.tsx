eb8f92a7bce1e77b52a222d42cca76f6e38d9233
Aqib Mahfuz <aqib.mahfuz@duke.edu>
2021-11-22 21:19:26 -0500

    Reviews now show dates properly

--- src/components/restaurants/restaurantConstants.tsx
+++ src/components/restaurants/restaurantConstants.tsx
@@ -70,3 +70,15 @@ export function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => n
     });
     return stabilizedThis.map((el) => el[0]);
 }
+
+export function getFormattedDate(date: Date) {
+    var year = date.getFullYear();
+  
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+  
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    
+    return month + '/' + day + '/' + year;
+  }
